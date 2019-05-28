9031cc58792cdbbf6fa5563593fe2c3e641c853d
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-05-28 14:53:11 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Helmet } from 'react-helmet';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Cyder Assessment</title>
+
+			</Helmet>
+			<h1>Hello</h1>
+		</div>
+	);
 }
 
 export default App;
